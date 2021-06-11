c376f41befc6e5f9f647838f88940f6585f61d60
FarazAfridi392 <[farazafridi697@gmail.com]>
2021-06-12 04:03:30 +0500

    changes

--- lib/main.dart
+++ lib/main.dart
@@ -50,7 +50,7 @@ class _MyHomePageState extends State<MyHomePage>
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text("Animation"),
+        title: Text("Flutter Animation"),
       ),
       body: Container(
         height: MediaQuery.of(context).size.height * 1,
