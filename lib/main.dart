205e08343f4118f0456bf9f8ced806e036e49e56
FarazAfridi392 <[farazafridi392@gmail.com]>
2021-06-12 03:14:02 +0500

    ufo_animation

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,5 @@
+import 'dart:math';
+
 import 'package:auto_size_text/auto_size_text.dart';
 import 'package:flutter/material.dart';
 import 'package:sizer/sizer.dart';
@@ -14,27 +16,100 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: FlutterAnimation(),
+      home: MyHomePage(),
     );
   }
 }
 
-class FlutterAnimation extends StatelessWidget {
-  const FlutterAnimation({Key? key}) : super(key: key);
+class MyHomePage extends StatefulWidget {
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage>
+    with SingleTickerProviderStateMixin {
+  final Image starsBackground = Image.asset(
+    'assets/Milky-Way.jpg',
+    fit: BoxFit.fill,
+  );
+  final Image ufo = Image.asset(
+    'assets/ufo.png',
+  );
+  late AnimationController _animation;
+
+  @override
+  void initState() {
+    super.initState();
+    _animation = AnimationController(
+      duration: const Duration(seconds: 5),
+      vsync: this,
+    )..repeat();
+  }
 
   @override
   Widget build(BuildContext context) {
-    final height = MediaQuery.of(context).size.height -
-        MediaQuery.of(context).padding.top -
-        kToolbarHeight;
-    final width = MediaQuery.of(context).size.width;
-    return SafeArea(
-      child: Scaffold(
-        appBar: AppBar(
-          title: Text("Responsive Screen"),
+    return Scaffold(
+      body: Container(
+        height: MediaQuery.of(context).size.height * 1,
+        decoration: BoxDecoration(
+          image: DecorationImage(
+              image: AssetImage(
+                "assets/Milky-Way.jpg",
+              ),
+              fit: BoxFit.fill),
         ),
-        body: Center(),
+        child: Stack(
+          alignment: AlignmentDirectional.center,
+          children: <Widget>[
+            AnimatedBuilder(
+              animation: _animation,
+              builder: (_, __) {
+                return ClipPath(
+                  clipper: const BeamClipper(),
+                  child: Container(
+                    height: 1000,
+                    decoration: BoxDecoration(
+                      gradient: RadialGradient(
+                        radius: 1.5,
+                        colors: [
+                          Colors.yellow,
+                          Colors.transparent,
+                        ],
+                        stops: [0, _animation.value],
+                      ),
+                    ),
+                  ),
+                );
+              },
+            ),
+            ufo,
+          ],
+        ),
       ),
     );
   }
+
+  @override
+  void dispose() {
+    _animation.dispose();
+    super.dispose();
+  }
+}
+
+class BeamClipper extends CustomClipper<Path> {
+  const BeamClipper();
+
+  @override
+  getClip(Size size) {
+    return Path()
+      ..lineTo(size.width / 2, size.height / 2)
+      ..lineTo(size.width, size.height)
+      ..lineTo(0, size.height)
+      ..lineTo(size.width / 2, size.height / 2)
+      ..close();
+  }
+
+  /// Return false always because we always clip the same area.
+  @override
+  bool shouldReclip(CustomClipper oldClipper) => false;
 }
